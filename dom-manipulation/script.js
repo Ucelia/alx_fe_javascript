e29f7d11b0aa6dd9c66970c9c06708db948d93d8
celia <neevecelia@gmail.com>
2024-10-23 21:03:29 +0200

    corrected some errors

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', createAddQuoteForm);
+
 let quotes = [
     {text:"The way to get started is to quit talking and begin doing.", category:"Motivational Quotes"},
     {text:"The purpose of our lives is to be happy.", category:"Life Quotes"},
@@ -6,17 +8,6 @@ let quotes = [
 ]
 const newQuoteBtn = document.getElementById('newQuote');
 const quoteDisplay = document.getElementById('quoteDisplay');
-const newQuoteText = document.getElementById('newQuoteText').value.trim();
-const newQuoteCategory = document.getElementById('newQuoteCategory').value.trim();
-const addQuoteDyn = `
-<div>
-        <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
-        <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
-        <button onclick="addQuote()">Add Quote</button>
-      </div> 
-`;
-
-quoteDisplay.innerHTML += addQuoteDyn;
 
 function showRandomQuote(){
     const randomIndex = Math.floor(Math.random() * quotes.length);
@@ -28,20 +19,28 @@ function showRandomQuote(){
 newQuoteBtn.addEventListener('click', showRandomQuote);
 
 function createAddQuoteForm(){
+    const addQuoteDyn = `
+<div>
+        <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
+        <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
+        <button onclick="addQuote()">Add Quote</button>
+      </div> 
+`;
     quoteDisplay.innerHTML += addQuoteDyn;
 }
 
-document.addEventListener('DOMContentLoaded', createAddQuoteForm);
 
 function addQuote(){
+    const newQuoteText = document.getElementById('newQuoteText').value.trim();
+    const newQuoteCategory = document.getElementById('newQuoteCategory').value.trim();
     if (newQuoteText ==='' || newQuoteCategory === ''){
         alert ('Fill in all the fields')
         return;
     }
     quotes.push({text: newQuoteText,category: newQuoteCategory});
     const quoteList = document.createElement('li');
-    quoteList.textContent = quotes;
+    quoteList.textContent = `${newQuoteText} - [${newQuoteCategory}]`;
     quoteDisplay.appendChild(quoteList);
-    newQuoteText ==='';
-    newQuoteCategory ==='';
+    newQuoteText ='';
+    newQuoteCategory ='';
 }
