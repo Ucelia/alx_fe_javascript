7fcc86338b90a736d5ccf742575feb0861455097
celia <neevecelia@gmail.com>
2024-10-24 03:13:30 +0200

    Implementing Web Storage and JSON Handling

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', createAddQuoteForm);
 
-let quotes = [
+let quotes = JSON.parse(localStorage.getItem('quotes')) || [
     {text:"The way to get started is to quit talking and begin doing.", category:"Motivational Quotes"},
     {text:"The purpose of our lives is to be happy.", category:"Life Quotes"},
     {text:"Everything you can imagine is real.", category:"Success"},
@@ -25,6 +25,7 @@ function createAddQuoteForm(){
         <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
         <button onclick="addQuote()">Add Quote</button>
       </div> 
+      <input type="file" id="importFile" accept=".json" onchange="importFromJsonFile(event)" />
 `;
     quoteDisplay.innerHTML += addQuoteDyn;
 }
@@ -33,6 +34,11 @@ function createAddQuoteForm(){
 function addQuote(){
     const newQuoteText = document.getElementById('newQuoteText').value.trim();
     const newQuoteCategory = document.getElementById('newQuoteCategory').value.trim();
+    // const importFromJsonBtn = `
+    // <input type="file" id="importFile" accept=".json" onchange="importFromJsonFile(event)" />
+    // `;
+    // quoteDisplay.innerHTML += importFromJsonBtn;
+
     if (newQuoteText ==='' || newQuoteCategory === ''){
         alert ('Fill in all the fields')
         return;
@@ -41,6 +47,30 @@ function addQuote(){
     const quoteList = document.createElement('li');
     quoteList.textContent = `${newQuoteText} - [${newQuoteCategory}]`;
     quoteDisplay.appendChild(quoteList);
+
+    
+    saveQuotes();
+
+    //localStorage.setItem();
     newQuoteText ='';
     newQuoteCategory ='';
 }
+
+function saveQuotes(quotes) {
+        const storedQuotes = JSON.parse(localStorage.getItem('quotes') || '[]');
+        storedQuotes.push(quotes);
+        localStorage.setItem('quotes', JSON.stringify(storedQuotes));
+    }
+
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
+
+const jsonString = JSON.stringify(quotes);
